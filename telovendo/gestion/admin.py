43a8ff8297f40cca3e9641da228df0f4c3ed5b6f
Francisco Roldán <francisco.roldang@live.com>
2023-05-18 20:53:22 -0400

    feat: modelo de usuario personalizado, login por email

--- telovendo/gestion/admin.py
+++ telovendo/gestion/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from .models import Usuario
+
+
+@admin.register(Usuario)
+class UserAdmin(DjangoUserAdmin):
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('id', 'email', 'is_staff')
+    search_fields = ('email',)
+    ordering = ('email',)
